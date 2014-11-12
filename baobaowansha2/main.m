365b87a046624909430566254c6dd6f8b02ca741
刘昕 <liuxin@liuxindeMacBook-Air.local>
2014-11-12 09:28:09 +0800

    Initial Commit

---
+++ baobaowansha2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  baobaowansha2
+//
+//  Created by 刘昕 on 14/11/12.
+//  Copyright (c) 2014年 刘昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ baobaowansha2Tests/baobaowansha2Tests.m
@@ -0,0 +1,40 @@
+//
+//  baobaowansha2Tests.m
+//  baobaowansha2Tests
+//
+//  Created by 刘昕 on 14/11/12.
+//  Copyright (c) 2014年 刘昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface baobaowansha2Tests : XCTestCase
+
+@end
+
+@implementation baobaowansha2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
