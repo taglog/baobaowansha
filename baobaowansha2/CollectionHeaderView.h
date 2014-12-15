df765ca096a2194afb547f4a1f71ed74a242497e
PanYongfeng <pan_yong_feng@hotmail.com>
2014-12-15 12:52:59 +0800

    add collection view cell

---
+++ baobaowansha2/CollectionHeaderView.h
@@ -0,0 +1,13 @@
+//
+//  CollectionHeaderView.h
+//  baobaowansha2
+//
+//  Created by PanYongfeng on 14/12/15.
+//  Copyright (c) 2014年 刘昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CollectionHeaderView : UICollectionReusableView
+
+@end
---
+++ baobaowansha2/CollectionHeaderView.m
@@ -0,0 +1,13 @@
+//
+//  CollectionHeaderView.m
+//  baobaowansha2
+//
+//  Created by PanYongfeng on 14/12/15.
+//  Copyright (c) 2014年 刘昕. All rights reserved.
+//
+
+#import "CollectionHeaderView.h"
+
+@implementation CollectionHeaderView
+
+@end
